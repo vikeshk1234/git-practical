72107617e0f32fcde32ea84103c668f9feaeccb7
vikeshk1234 <coderp33@gmail.com>
2022-09-05 11:17:41 +0530

    inital commit

---
+++ newseletter-sighup/app.js
@@ -0,0 +1,6 @@
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const express= require("express");
+const { application } = require("express");
+app.express = express();
